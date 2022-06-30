80f8966d86aab2430eb6ab500ae656be037fd2ff
Kaio Marx <kaiomarxdev@gmail.com>
2022-06-30 19:22:37 -0300

    feat: add the grid layout to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import styles from './app.module.css';
 import { LeftMenu } from './Components/LeftMenu';
 import './global.css';
 
+
 export function App() {
   return (
-    <LeftMenu />
+    <div className={styles.homepage}>
+      <LeftMenu />
+
+    </div>
   )
 }
