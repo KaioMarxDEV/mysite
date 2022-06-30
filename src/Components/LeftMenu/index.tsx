277a07181eebe6be507ea2cf17c0ff75b94be7ff
Kaio Marx <kaiomarxdev@gmail.com>
2022-06-30 15:05:01 -0300

    feat: left menu component

--- src/Components/LeftMenu/index.tsx
+++ src/Components/LeftMenu/index.tsx
@@ -1,4 +1,4 @@
-import { GithubLogo, LinkedinLogo } from 'phosphor-react';
+import { CheckCircle, GithubLogo, LinkedinLogo } from 'phosphor-react';
 import styles from './leftMenu.module.css';
 
 export function LeftMenu() {
@@ -6,18 +6,56 @@ export function LeftMenu() {
     <div className={styles.leftmenu}>
       <div className={styles.profile}>
         <img src="http://github.com/kaiomarxdev.png" alt="Kaio Marx picture" />
-        <strong>Kaio Marx</strong>
+        <strong>Kaio Marx Alves</strong>
         <span>Front End Developer</span>
 
         <div className={styles.socialwrapper}>
-          <a href="#">
+          <a href="https://github.com/KaioMarxDEV" target='_blank'>
             <GithubLogo size={24} />
           </a>
-          <a href="#">
+          <a href="https://www.linkedin.com/in/kaiomarxdev/" target='_blank'>
             <LinkedinLogo size={24} />
           </a>
         </div>
       </div>
+
+      <div className={styles.separator} />
+
+      <div className={styles.about}>
+        <div>
+          <span>Age: </span>
+          <p>20</p>
+        </div>
+        <div>
+          <span>Remote:</span>
+          <p className={styles.avaiable}>Avaiable</p>
+        </div>
+        <div>
+          <span>Full Time: </span>
+          <p className={styles.avaiable}>Avaiable</p>
+        </div>
+        <div>
+          <span>Address: </span>
+          <p>Brazil</p>
+        </div>
+      </div>
+
+      <div className={styles.separator} />
+
+      <div className={styles.languages}>
+        <div>
+          <span>English</span>
+          <div className={styles.languagesSeparator}/>
+          <CheckCircle size={24} />
+        </div>
+        <div>
+          <span>Portuguese</span>
+          <div className={styles.languagesSeparator}/>
+          <CheckCircle size={32} />
+        </div>
+      </div>
+
+      <div className={styles.separator} />
     </div>
   )
 }
