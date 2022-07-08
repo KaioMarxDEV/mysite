92d55487825950e73cf9f6a96b57e82b90ca5773
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-08 10:29:04 -0300

    fix: reorganize spaces

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,7 +3,7 @@ import { HandWaving } from "phosphor-react";
 export function Header() {
   return (
     <header className="flex flex-row h-20 justify-between mt-8 items-center bg-gradient-to-b from-white/20 to-white/60 px-4 rounded-lg">
-      <h1 className="box-decoration-clone text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-pink-500 underline px-4 py-2 text-3xl font-bold">
+      <h1 className="text-my-blue px-4 py-2 text-3xl font-bold">
         Welcome to my Portfolio
       </h1>
       <div className="flex flex-row items-center">
