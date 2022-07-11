2a31324aedc97d704f2e234bbfeb2d06772167c5
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-11 09:58:52 -0300

    change: projects title color to gradient

--- src/Components/Projects/index.tsx
+++ src/Components/Projects/index.tsx
@@ -18,7 +18,10 @@ export function Projects() {
     <div className="my-56 w-full flex flex-col items-center">
       <div className="flex flex-col items-center">
         <h1 className="mb-4 text-center text-5xl font-bold text-black drop-shadow-xl">
-          React.js Case Study
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-pink-500">
+            React.js
+          </span>{' '}
+          Case Study
         </h1>
         <span className="text-center text-gray-900">
           Some of my case studies, Based on personal projects, challenges, etc.
