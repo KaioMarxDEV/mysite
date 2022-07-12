09ef9ece49ba186d10c1ecf6ea8d512fe44edde6
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-12 07:01:22 -0300

    add new front end skill

--- src/utils/frontend.ts
+++ src/utils/frontend.ts
@@ -2,9 +2,10 @@ export default [
   {id: 1, name: 'Redux'},
   {id: 2, name: 'React'},
   {id: 3, name: 'Hooks'},
-  {id: 4, name: 'Javascript'},
-  {id: 5, name: 'Typescript'},
-  {id: 6, name: 'HTML, CSS'},
-  {id: 7, name: 'CMS'},
-  {id: 8, name: 'Unit Test'}
+  {id: 4, name: 'Styled-components'},
+  {id: 5, name: 'Javascript'},
+  {id: 6, name: 'Typescript'},
+  {id: 7, name: 'HTML, CSS'},
+  {id: 8, name: 'CMS'},
+  {id: 9, name: 'Unit Test'}
 ]
