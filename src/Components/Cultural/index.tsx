d3997337942f884a5e6d6ababba373195bd0bdca
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-15 17:58:40 -0300

    fix: redo cultural fit colors

--- src/Components/Cultural/index.tsx
+++ src/Components/Cultural/index.tsx
@@ -11,33 +11,33 @@ export function Cultural() {
         </strong>
       </div>
       <div className="flex w-full flex-col items-start">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
           The company willingly takes risks in hiring different people, which wins them the leadership 
           position in the industry.
         </p>
       </div>
       <div className="my-1 flex w-full flex-col items-end">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
           Conflicts are not avoided but approached with trained skills and resolved well.
         </p>
       </div>
       <div className="flex w-full flex-col items-start">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
           There is a high level of trust, and ideas flow freely.
         </p>
       </div>
       <div className="mt-1 flex w-full flex-col items-end">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
         The company as also the people who build it, share the enjoyment of embracing the change.
         </p>
       </div>
       <div className="my-1 flex w-full flex-col items-start">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
         The company motivates as a core value their people to take initiative, it is a satisfaction to have more challenges to resolve.
         </p>
       </div>
       <div className="flex w-full flex-col items-end">
-        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-my-blue border-dashed hover:border-solid hover:cursor-pointer transition-all">
+        <p className="w-1/2 px-4 py-2 items-start text-base text-gray-700 border rounded-lg border-gray-300">
         Detail oriented is buzz word now, but it describes at least how I see my work, as every detail counts in the experience of the user 
         I have to be concerned about it to be able to deliver the best.
         </p>
